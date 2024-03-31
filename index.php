b0bbb16b243eed7dfde0cbfab860edaa668be440
Ananda Ardiansyah Hariaji <rezalol333@gmail.com>
2024-03-31 22:38:19 +0700

    link

--- index.php
+++ index.php
@@ -27,33 +27,34 @@
       </head>
 
       <body>
-        <nav class="bg-purple-700">
-          <div class="max-w-7xl">
-            <div class=" flex h-16 items-center">
-  
-              <div class="flex flex-1 ">
-                <div class="flex flex-shrink-0">
-                  <img class="h-8 w-auto" src="CareSelf.png" alt="Your Company" onclick="window.location.href = 'index.php';">
-                </div>
-              </div>
-  
-                      <div class="flex flex-2 space-x-4">
-     
-                       <a href="./index.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                       <a href="./List_Produk.html" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Product</a>
-                       <a href="./login.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
-     
-                       </div>
-                       
-                  </div>
-        
-          
-                  
-                </div>
-          </div>
-        
+      <nav class="bg-purple-700">
+        <div class="max-w-7xl">
+          <div class=" flex h-16 items-center">
 
-        </nav>
+            <div class="flex flex-1 ">
+              <div class="flex flex-shrink-0">
+                <img class="h-8 w-auto" src="CareSelf.png" alt="Your Company" onclick="window.location.href = 'index.php';">
+              </div>
+            </div>
+
+            <div class="flex flex-2 space-x-4">
+   
+              <a href="./index.php" class=" text-white hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="./list_produk.php" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Product</a>
+              <a href="./dashboard.php" class="bg-gray-900 text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
+              <a href="logout.php" class="bg-red-600 text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Logout</a>
+
+              </div>
+                     
+                </div>
+      
+                
+                
+              </div>
+        </div>
+      
+       
+      </nav>
 
         <script>
           var navItems = document.getElementById("navItems");
@@ -95,7 +96,7 @@
 
               <p class="mt-2 text-sm text-gray-700 md:text-lg">Let's care about your hair like you care about your style.</p>
               <div class="flex space-x-3 justify-center lg:justify-start mt-6"></div>
-              <button class="rounded-full bg-purple-700 p-3 text-white" onclick="window.location.href = 'List_Produk.html';">Check Now</button>
+              <button class="rounded-full bg-purple-700 p-3 text-white" onclick="window.location.href = 'list_produk.php';">Check Now</button>
             </div>
           </div>
           <!-- <div class="hidden lg:block w-full lg:h-screen">
