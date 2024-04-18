e3c5196669fecb4d4dc862e60c55d96feaaa712c
Farrel Aqeel Danendra <farrqeel@gmail.com>
2024-04-18 23:12:59 +0700

    ganti sql

--- sql/manage_perusahaan.sql
+++ sql/manage_perusahaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 31 Mar 2024 pada 17.35
+-- Waktu pembuatan: 18 Apr 2024 pada 18.12
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.1.25
 
@@ -42,8 +42,7 @@ CREATE TABLE `account` (
 
 INSERT INTO `account` (`id`, `name`, `username`, `email`, `password`, `role_id`) VALUES
 (5, 'admin', 'admin', 'admin@gmail.com', '$2y$10$oa5JzeaWLygRQXyL5zk2peaxjFWvMH7ovNkugaxvItbau05ilLzAu', 1),
-(10, 'admin', 'admin2', 'farrqeel@gmail.com', '$2y$10$AW1oDknHStzNkuSyaTFdjee5tYFugO3yLowrK8D4weNpOFeoL0VaG', 1),
-(11, 'admin', 'admin3', 'farrel.pok@gmail.com', '$2y$10$7OA6WIxaD.uqdmWgh7fRwulhYLk0mmPvFhtKsDQlhwxNOIltoSppe', 1);
+(12, 'customer', 'customer', 'customer@gmail.com', '$2y$10$Ir8FdYsuFE/cYXhRkxzjNeTjliVhsCmo2zEEg9nJ5O6R5zdE55Pwq', 2);
 
 -- --------------------------------------------------------
 
@@ -65,6 +64,59 @@ CREATE TABLE `customer` (
   `bukti_bayar` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_order`
+--
+
+CREATE TABLE `detail_order` (
+  `order_id` int(11) NOT NULL,
+  `produk_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `detail_order`
+--
+
+INSERT INTO `detail_order` (`order_id`, `produk_id`, `qty`) VALUES
+(22, 11, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `number` varchar(50) NOT NULL,
+  `adress` varchar(255) NOT NULL,
+  `shipping_method` varchar(100) NOT NULL,
+  `harga` int(128) NOT NULL,
+  `status` int(11) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `orders`
+--
+
+INSERT INTO `orders` (`id`, `tanggal`, `name`, `email`, `number`, `adress`, `shipping_method`, `harga`, `status`, `foto`) VALUES
+(22, '2024-04-18', 'Farrel Aqeel Danendra', 'farrqeel@gmail.com', '088102682612', 'Jalan Petemon 12', 'express', 60000, 2, 'img/66213b802124a.jpeg');
+
+--
+-- Trigger `orders`
+--
+DELIMITER $$
+CREATE TRIGGER `delete` AFTER DELETE ON `orders` FOR EACH ROW DELETE FROM detail_order WHERE order_id = OLD.id
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -111,7 +163,8 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id`, `tanggal`, `nama_produk`, `harga`, `stok`, `produk_terjual`, `deskripsi`, `foto`) VALUES
-(3, '2024-03-28', 'Shampoo', 10000, 170, 1, '1', '');
+(10, '2024-04-18', 'Sabun Mandi ', 12000, 15, 0, 'Sabun mandi oke', 0x696d672f363632313335663936343835662e6a7067),
+(11, '2024-04-18', 'Shampo', 15000, 3, 0, 'Shampoo oke', 0x696d672f363632313336313037663064652e6a7067);
 
 -- --------------------------------------------------------
 
@@ -153,6 +206,12 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `persediaan`
 --
 ALTER TABLE `persediaan`
@@ -178,13 +237,19 @@ ALTER TABLE `produksi`
 -- AUTO_INCREMENT untuk tabel `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `customer`
 --
 ALTER TABLE `customer`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT untuk tabel `persediaan`
@@ -196,7 +261,7 @@ ALTER TABLE `persediaan`
 -- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `produksi`
